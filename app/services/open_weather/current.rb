a9806805c4e59116879f921d4890a791ff12e11d
Bryan Moreno <morenobryan91@gmail.com>
2017-07-22 21:52:28 -0300

    Add method to retrieve weather from coordinates and random coordinates

--- app/services/open_weather/current.rb
+++ app/services/open_weather/current.rb
@@ -7,5 +7,19 @@ module OpenWeather
       )
       handle_response(response)
     end
+
+    def random
+      latitude = Random.rand(-90.0..90.0)
+      longitude = Random.rand(-180.0..180.0)
+      coordinates(latitude: latitude, longitude: longitude)
+    end
+
+    def coordinates(latitude:, longitude:)
+      response = self.class.get(
+        '/weather',
+        query: @options.merge(lat: latitude, lon: longitude)
+      )
+      handle_response(response)
+    end
   end
 end
--- spec/services/open_weather/current_spec.rb
+++ spec/services/open_weather/current_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe OpenWeather::Current do
   let(:open_weather) { described_class.new }
-  let(:city) { 'Curitiba' }
-  let(:country) { 'BR' }
 
   describe '#city' do
+    let(:city) { 'Curitiba' }
+    let(:country) { 'BR' }
+
     describe 'correct parameters' do
       it 'returns the object with the right parameters',
          vcr: { cassette_name: 'open_weather_valid_city' } do
@@ -50,6 +51,50 @@ RSpec.describe OpenWeather::Current do
         it 'returns an error message with code',
            vcr: { cassette_name: 'open_weather_empty_city' } do
           response = open_weather.city(city: '')
+          expect(response.cod).to eql '400'
+          expect(response.message).to eql 'Nothing to geocode'
+        end
+      end
+    end
+  end
+
+  describe '#coordinates' do
+    describe 'correct parameters' do
+      it 'returns the object with the right parameters',
+         vcr: { cassette_name: 'open_weather_valid_coordinates' } do
+        response = open_weather.coordinates(latitude: -49.29, longitude: -25.5)
+        expect(response.cod).to eql 200
+        expect(response).to respond_to(:coord)
+        expect(response).to respond_to(:weather)
+        expect(response).to respond_to(:base)
+        expect(response).to respond_to(:main)
+        expect(response).to respond_to(:wind)
+        expect(response).to respond_to(:clouds)
+        expect(response).to respond_to(:sys)
+      end
+    end
+
+    describe 'errors' do
+      context 'invalid coordinates' do
+        it 'returns an error message if the latitude is invalid',
+           vcr: { cassette_name: 'open_weather_invalid_latitude' } do
+          response = open_weather.coordinates(latitude: -91, longitude: 0)
+          expect(response.cod).to eql '400'
+          expect(response.message).to eql '-91 is not a float'
+        end
+
+        it 'returns an error message with code',
+           vcr: { cassette_name: 'open_weather_invalid_longitude' } do
+          response = open_weather.coordinates(latitude: 0, longitude: -181)
+          expect(response.cod).to eql '400'
+          expect(response.message).to eql '-181 is not a float'
+        end
+      end
+
+      context 'empty coordinates' do
+        it 'returns an error message with code',
+           vcr: { cassette_name: 'open_weather_empty_coordinates' } do
+          response = open_weather.coordinates(latitude: nil, longitude: nil)
           expect(response.cod).to eql '400'
           expect(response.message).to eql 'Nothing to geocode'
         end
