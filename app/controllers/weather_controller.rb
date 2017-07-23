7f73a4108dd0f66a6bdac2447d6c3ee5002251a6
Bryan Moreno <morenobryan91@gmail.com>
2017-07-23 00:38:45 -0300

    Refine controller actions to handle parameters and call services

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,7 +1,20 @@
 class WeatherController < ApplicationController
   def random
+    @weather = OpenWeather::Current.new.random
+    render 'weather'
   end
 
   def city
+    @weather = OpenWeather::Current.new.city(
+      city: weather_params[:city],
+      country: weather_params[:country]
+    )
+    render 'weather'
+  end
+
+  private
+
+  def weather_params
+    params.fetch(:weather, {})
   end
 end
