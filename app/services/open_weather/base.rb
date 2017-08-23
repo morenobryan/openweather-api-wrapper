9f924406099f12fdade370216594cf0ad75c2f4d
Bryan Moreno <morenobryan91@gmail.com>
2017-08-23 20:01:31 -0300

    Improve the API of the weather result object

--- app/services/open_weather/base.rb
+++ app/services/open_weather/base.rb
@@ -11,7 +11,24 @@ module OpenWeather
     end
 
     def handle_response(response)
-      JSON.parse(response.body, object_class: OpenStruct)
+      format_response(JSON.parse(response.body, object_class: OpenStruct))
+    end
+
+    private
+
+    def format_response(parsed_response)
+      return parsed_response if parsed_response.cod != 200
+      parsed_response.pressure = parsed_response.main.pressure
+      parsed_response.humidity = parsed_response.main.humidity
+      parsed_response.temp = parsed_response.main.temp
+      parsed_response.temp_max = parsed_response.main.temp_max
+      parsed_response.temp_min = parsed_response.main.temp_min
+      parsed_response.clouds = parsed_response.clouds.all
+      parsed_response.datetime = parsed_response.dt
+      parsed_response.icon = parsed_response.weather.first.icon
+      parsed_response.forecast = parsed_response.weather.first.main
+      parsed_response.description = parsed_response.weather.first.description
+      parsed_response
     end
   end
 end
