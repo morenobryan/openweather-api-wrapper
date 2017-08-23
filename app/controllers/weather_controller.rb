d74b8e263929b50c6a1be0325e4c0633e276a2ec
Bryan Moreno <morenobryan91@gmail.com>
2017-08-22 22:03:17 -0300

    Add caching support for city and random searching

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,14 +1,31 @@
 class WeatherController < ApplicationController
   def random
-    @weather = OpenWeather::Current.new.random
+    latitude = Random.rand(-90.0..90.0)
+    longitude = Random.rand(-180.0..180.0)
+    cache_key = "coordinates/#{latitude}/#{longitude}"
+
+    @weather = Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
+      OpenWeather::Current.new.coordinates(
+        latitude: latitude,
+        longitude: longitude
+      )
+    end
+
     render 'weather'
   end
 
   def city
-    @weather = OpenWeather::Current.new.city(
-      city: weather_params[:city],
-      country: weather_params[:country]
-    )
+    city = weather_params[:city]
+    country = weather_params[:country]
+    cache_key = "city/#{city}/#{country}"
+
+    @weather = Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
+      OpenWeather::Current.new.city(
+        city: city,
+        country: country
+      )
+    end
+
     render 'weather'
   end
 
